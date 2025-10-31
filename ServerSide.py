bf46684cb488de728caa4ad144a69a7f4ba152a7
cdtayl19 <cdtayl19@asu.edu>
2025-10-31 16:13:48 -0400

    login frame

--- ServerSide.py
+++ ServerSide.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/api', methods=["GET"])
-def greet():
-    return jsonify({"message": "bye"})
+@app.route("/")
+def home():
+    return render_template("Login.html")
 
 if __name__ == "__main__":
     app.run(port=8080)
