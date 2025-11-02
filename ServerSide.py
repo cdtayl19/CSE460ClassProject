51a3b466dc70ae8306741e23a1e6b13ea4e538e2
cdtayl19 <cdtayl19@asu.edu>
2025-11-02 15:00:27 -0500

    Add csv to hold created accounts, a default admin account, newly created accounts are added to csv

--- ServerSide.py
+++ ServerSide.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, jsonify
-import pandas as pd
+import csv
 
+
+# CSV Stuff
+FILE_NAME = "User_Accounts.csv"
+
+
+# Server Stuff
 app = Flask(__name__)
-
-
-# Variables
-new_account = {}
-
-
 
 @app.route("/")
 def home():
@@ -18,19 +18,14 @@ def createUser():
     if request.method == "POST":
         user_data = request.get_json()
 
-        firstname   = user_data["firstname"]
-        lastname    = user_data["lastname"]
-        email       = user_data["email"]
-        username    = user_data["username"]
-        password    = user_data["password"]
-        role        = user_data["role"]
-        
-        # Stores user data into larger scope variable to move to pandas data frame.
-        new_account = user_data
+        # Send user data to csv for storage
+        with open(FILE_NAME, 'a', newline='') as new_file:
+            fieldnames = ['firstname', 'lastname', 'email', 'username', 'password', 'role']
+            csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=',')
+            csv_writer.writerow(user_data)
 
-        # STORE NEW USER DATA IN A PANDAS DATA FRAME SEE HOMEWORK 3 FROM 416
-        print("Received new account:", new_account)
-        return jsonify({"status": "success", "message": f"User {username} created!"})
+        print("Received new account:", user_data)
+        return jsonify({"status": "success", "message": f"User {user_data["username"]} created!"})
     
     return render_template("CreateUser.html")
 
