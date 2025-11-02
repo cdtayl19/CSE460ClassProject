37e2ea7e0da48ee6e316b0ae611f3645758613c3
cdtayl19 <cdtayl19@asu.edu>
2025-11-02 16:33:46 -0500

    Add checks for existing email and username, add pandas dataframe, made writing to csv a reusable method

--- ServerSide.py
+++ ServerSide.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
 import csv
 
 
 # CSV Stuff
 FILE_NAME = "User_Accounts.csv"
+
+# Read in data from csv
+def csv_read(file) -> list:
+    with open(file, 'r') as data:
+        csv_reader = csv.DictReader(data)
+        results = [row for row in csv_reader]
+    return results
+
+# Write data to csv
+def csv_write(user_data):
+    with open(FILE_NAME, 'a', newline='') as new_file:
+        fieldnames = ['firstname', 'lastname', 'email', 'username', 'password', 'role']
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=',')
+        csv_writer.writerow(user_data)
 
 
 # Server Stuff
@@ -18,7 +33,7 @@ def createUser():
     if request.method == "POST":
         user_data = request.get_json()
 
-        # Empty filed checks
+        # Empty field checks
         if user_data["firstname"] == "":
             return jsonify({"status": "fail", "message": "First name cannot be empty."})
         if user_data["lastname"] == "":
@@ -29,12 +44,20 @@ def createUser():
             return jsonify({"status": "fail", "message": "Username cannot be empty."})
         if user_data["password"] == "":
             return jsonify({"status": "fail", "message": "Password cannot be empty."})
+        
+        # Existing account checks
+        df = pd.read_csv(FILE_NAME)
+
+        # Existing email check
+        if user_data["email"] in df["Email"].values:
+            return jsonify({"status": "fail", "message": "Account with that email already exists."})
+        
+        # Existing username check
+        if user_data["username"] in df["Username"].values:
+            return jsonify({"status": "fail", "message": "Account with that username already exists."})
 
         # Send user data to csv for storage
-        with open(FILE_NAME, 'a', newline='') as new_file:
-            fieldnames = ['firstname', 'lastname', 'email', 'username', 'password', 'role']
-            csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=',')
-            csv_writer.writerow(user_data)
+        csv_write(user_data)
 
         print("Received new account:", user_data)
         return jsonify({"status": "success", "message": f"User {user_data["username"]} created!"})
