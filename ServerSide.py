077bc34f9e6e00866f7fcc5a3a760310d3039f43
cdtayl19 <cdtayl19@asu.edu>
2025-11-01 22:00:14 -0400

    Server recieves user input data from create account

--- ServerSide.py
+++ ServerSide.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -6,8 +8,21 @@ app = Flask(__name__)
 def home():
     return render_template("Login.html")
 
-@app.route("/create-account")
+@app.route("/create-account", methods=["GET", "POST"])
 def createUser():
+    if request.method == "POST":
+        user_data = request.get_json()
+
+        firstname   = user_data["firstname"]
+        lastname    = user_data["lastname"]
+        email       = user_data["email"]
+        username    = user_data["username"]
+        password    = user_data["password"]
+
+        # STORE NEW USER DATA IN A PANDAS DATA FRAME SEE HOMEWORK 3 FROM 416
+        print("Received new account:", user_data)
+        return jsonify({"status": "success", "message": f"User {username} created!"})
+    
     return render_template("CreateUser.html")
 
 if __name__ == "__main__":
