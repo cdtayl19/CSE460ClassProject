52b63891987e0582dde205580e87cd5092e11db7
cdtayl19 <cdtayl19@asu.edu>
2025-11-02 14:05:41 -0500

    Add role to user account

--- ServerSide.py
+++ ServerSide.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 
-
 app = Flask(__name__)
+
+
+# Variables
+new_account = {}
+
+
 
 @app.route("/")
 def home():
@@ -18,9 +23,13 @@ def createUser():
         email       = user_data["email"]
         username    = user_data["username"]
         password    = user_data["password"]
+        role        = user_data["role"]
+        
+        # Stores user data into larger scope variable to move to pandas data frame.
+        new_account = user_data
 
         # STORE NEW USER DATA IN A PANDAS DATA FRAME SEE HOMEWORK 3 FROM 416
-        print("Received new account:", user_data)
+        print("Received new account:", new_account)
         return jsonify({"status": "success", "message": f"User {username} created!"})
     
     return render_template("CreateUser.html")
